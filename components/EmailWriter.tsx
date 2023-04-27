9c264b3352fed7823a217642d85a12f664499384
Wei-Che Chung <wchung@seattleu.edu>
2023-04-27 14:28:49 -0700

    Create EmailWriter component

---
+++ components/EmailWriter.tsx
@@ -0,0 +1,14 @@
+import Button from "@/components/Button";
+
+type EmailWriterProps = {};
+
+const EmailWriter = (props: EmailWriterProps) => {
+  return (
+    <div className="mt-10">
+      Email
+      <input type="text" />
+    </div>
+  );
+};
+
+export default EmailWriter;
