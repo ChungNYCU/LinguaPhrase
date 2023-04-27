3aad30e4d0433124bfdd2d3bbe700983a400e97c
Wei-Che Chung <wchung@seattleu.edu>
2023-04-27 14:29:00 -0700

    Create GrammarFixer component

---
+++ components/GrammarFixer.tsx
@@ -0,0 +1,14 @@
+import Button from "@/components/Button";
+
+type GrammarFixerProps = {};
+
+const GrammarFixer = (props: GrammarFixerProps) => {
+  return (
+    <div className="mt-10">
+      Grammar
+      <input type="text" />
+    </div>
+  );
+};
+
+export default GrammarFixer;
