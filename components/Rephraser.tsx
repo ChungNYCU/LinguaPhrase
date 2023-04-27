970f193f4e855aabba21791569d81d1b61d98326
Wei-Che Chung <wchung@seattleu.edu>
2023-04-27 14:29:12 -0700

    Create Rephraser component

---
+++ components/Rephraser.tsx
@@ -0,0 +1,14 @@
+import Button from "@/components/Button";
+
+type RephraserProps = {};
+
+const Rephraser = (props: RephraserProps) => {
+  return (
+    <div className="mt-10">
+      Rephrase
+      <input type="text" />
+    </div>
+  );
+};
+
+export default Rephraser;
