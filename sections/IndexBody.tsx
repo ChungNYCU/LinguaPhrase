5ed48946a641f4d5f4a0426b8d2f706279eac7fc
Wei-Che Chung <wchung@seattleu.edu>
2023-04-28 23:52:53 -0700

    add declaration

--- sections/IndexBody.tsx
+++ sections/IndexBody.tsx
@@ -3,9 +3,21 @@ import FeatureSelector from "@/components/FeatureSelector";
 const IndexBody = () => {
   return (
     <section className="mt-5">
-      <h1 className="font-bold">
+      <h1 className="mb-10 font-bold">
         <span className="dark:text-blue-500">LinguaPhrase</span>
       </h1>
+      <div>
+        If the response time is too long, it could be due to an excessive number
+        of requests. To deploy your own service, consider cloning the project
+        from my GitHub and using your OpenAI API key. <br />
+        Link:{" "}
+        <a
+          href="https://github.com/ChungNYCU/LinguaPhrase"
+          className="text-blue-500 hover:text-blue-700"
+        >
+          https://github.com/ChungNYCU/LinguaPhrase
+        </a>
+      </div>
       <FeatureSelector></FeatureSelector>
     </section>
   );
