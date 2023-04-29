a2ccece080cecb093dd89208d0f40bf8d334e155
Wei-Che Chung <wchung@seattleu.edu>
2023-04-29 11:03:34 -0700

    rm declaration

--- sections/IndexBody.tsx
+++ sections/IndexBody.tsx
@@ -6,18 +6,6 @@ const IndexBody = () => {
       <h1 className="mb-10 font-bold">
         <span className="dark:text-blue-500">LinguaPhrase</span>
       </h1>
-      <div>
-        If the response time is too long, it could be due to an excessive number
-        of requests. To deploy your own service, consider cloning the project
-        from my GitHub and using your OpenAI API key. <br />
-        Link:{" "}
-        <a
-          href="https://github.com/ChungNYCU/LinguaPhrase"
-          className="text-blue-500 hover:text-blue-700"
-        >
-          https://github.com/ChungNYCU/LinguaPhrase
-        </a>
-      </div>
       <FeatureSelector></FeatureSelector>
     </section>
   );
