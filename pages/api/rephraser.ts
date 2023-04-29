20ccd252b74d069ef10901de862bfe81f7cffdb6
Wei-Che Chung <wchung@seattleu.edu>
2023-04-28 20:08:00 -0700

    implement rephraser api

---
+++ pages/api/rephraser.ts
@@ -0,0 +1,59 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import axios from "axios";
+import express from "express";
+
+const app = express();
+
+// Define a POST request handler
+app.post(
+  "/api/rephraser",
+  async (req: express.Request, res: express.Response) => {
+    const { paragraph } = req.body;
+    const prompt = `Rephrase this: '${paragraph}'`;
+    // Define the request payload
+    const request = {
+      model: "text-davinci-003",
+      prompt: prompt,
+      max_tokens: 500,
+      temperature: 0.5,
+      top_p: 1,
+      n: 1,
+      stream: false,
+      logprobs: null,
+    };
+
+    // Define the API endpoint URL
+    const apiUrl = "https://api.openai.com/v1/completions";
+
+    // Add your API key as a header
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    };
+
+    var result: string = "";
+    // Send the request to the API
+    await axios
+      .post(apiUrl, request, { headers })
+      .then((response) => {
+        result = response.data.choices[0].text;
+        console.log(result); // Prints the generated text
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    res.status(201).json(result);
+  }
+);
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  // Call the appropriate handler function based on the request method
+  switch (req.method) {
+    case "POST":
+      return app(req, res);
+  }
+
+  // If the request method is not allowed, return an error
+  res.status(405).json({ message: "Method not allowed" });
+}
